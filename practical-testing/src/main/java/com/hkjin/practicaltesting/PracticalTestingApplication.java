4905d2420b0c27c83eb3c26f65a95e17aaf127db
hkjin <wlsgusrb78@naver.com>
2024-10-14 16:07:40 +0900

    init

---
+++ practical-testing/src/main/java/com/hkjin/practicaltesting/PracticalTestingApplication.java
@@ -0,0 +1,13 @@
+package com.hkjin.practicaltesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalTestingApplication.class, args);
+	}
+
+}
